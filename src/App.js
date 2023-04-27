8e0fbbb91890ecbe9aadcc4492a58f9ef35df9e7
John_Galt <john.galt.prog@gmail.com>
2023-04-27 12:34:23 +0300

    create custom hook useCounter

--- src/App.js
+++ src/App.js
@@ -2,71 +2,90 @@ import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
 
-const Counter = (props) => {
-    const [counter, setCounter] = React.useState(props.counter);
+function useCounter(initial) {
+    const [counter, setCounter] = useState(initial);
 
     const incCounter = () => {
-      if (counter < 50) {
-        setCounter(counter => counter + 1)
-      }
-    }
+        if (counter < 5) {
+            setCounter((counter) => counter + 1);
+        }
+    };
 
     const decCounter = () => {
-      if (counter > -50) {
-        setCounter(counter => counter - 1)
-      }
-    }
+        if (counter > -5) {
+            setCounter((counter) => counter - 1);
+        }
+    };
 
     const rndCounter = () => {
-      setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0))
-    }
+        setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0));
+    };
 
     const resetCounter = () => {
-      setCounter(props.counter)
-    }
+        setCounter(initial);
+    };
+
+    return { counter, incCounter, decCounter, rndCounter, resetCounter };
+}
+
+const Counter = (props) => {
+    // const [counter, setCounter] = useState(props.counter);
+
+    const { counter, incCounter, decCounter, rndCounter, resetCounter } =
+        useCounter(props.counter);
+
+    // const rndCounter = () => {
+    //   setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0))
+    // }
+
+    // const resetCounter = () => {
+    //   setCounter(props.counter)
+    // }
 
     return (
-      <div className="component">
-        <div className="counter">{counter}</div>
-        <div className="controls">
-          <button onClick={incCounter}>INC</button>
-          <button onClick={decCounter}>DEC</button>
-          <button onClick={rndCounter}>RND</button>
-          <button onClick={resetCounter}>RESET</button>
+        <div className="component">
+            <div className="counter">{counter}</div>
+            <div className="controls">
+                <button onClick={incCounter}>INC</button>
+                <button onClick={decCounter}>DEC</button>
+                <button onClick={rndCounter}>RND</button>
+                <button onClick={resetCounter}>RESET</button>
+            </div>
         </div>
-      </div>
-    )
-}
+    );
+};
 
 const RndCounter = (props) => {
-    const [counter, setCounter] = React.useState(props.counter);
+    // const [counter, setCounter] = useState(props.counter);
 
-    const rndCounter = () => {
-      setCounter((Math.random() * (50 - -50) + -50).toFixed(0))
-    }
+    // const rndCounter = () => {
+    //   setCounter((Math.random() * (50 - -50) + -50).toFixed(0))
+    // }
 
-    const resetCounter = () => {
-      setCounter(props.counter)
-    }
+    // const resetCounter = () => {
+    //   setCounter(props.counter)
+    // }
+
+    const { counter, rndCounter, resetCounter } = useCounter(props.counter);
 
     return (
-      <div className="component">
-        <div className="counter">{counter}</div>
-        <div className="controls">
-          <button onClick={rndCounter}>RND</button>
-          <button onClick={resetCounter}>RESET</button>
+        <div className="component">
+            <div className="counter">{counter}</div>
+            <div className="controls">
+                <button onClick={rndCounter}>RND</button>
+                <button onClick={resetCounter}>RESET</button>
+            </div>
         </div>
-      </div>
-    )
-}
+    );
+};
 
 const App = () => {
     return (
         <>
-            <Counter counter={0}/>
-            <RndCounter counter={5}/>
+            <Counter counter={0} />
+            <RndCounter counter={5} />
         </>
-    )
-}
+    );
+};
 
 export default App;
